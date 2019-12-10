cee950c38cf6122d10cba390228868416be6c3c3
ogonzal87 <ogonzal87@gmail.com>
2019-12-09 22:40:27 -0800

    tweaking the animations

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 let mic;
 let initialMessage = document.getElementsByTagName("h1");
-let flares = document.getElementById("layer1");
-let leaf1 = document.getElementById("layer2");
-let unseen = document.getElementById("layer3");
-let leaf2 = document.getElementById("layer4");
-let leaf3 = document.getElementById("layer5");
-let hear = document.getElementById("layer6");
-let leaf4 = document.getElementById("layer7");
-let the = document.getElementById("layer8");
-let leaf5 = document.getElementById("layer9");
-let background = document.getElementById("layer10");
+let flares1 = document.getElementById("layer1");
+let flares2 = document.getElementById("layer2");
+let flares3 = document.getElementById("layer3");
+let mainPlanet = document.getElementById("layer4");
+let bluePlanet = document.getElementById("layer5");
+let yellowPlanet = document.getElementById("layer6");
+let purplePlanet = document.getElementById("layer7");
+let clouds = document.getElementById("layer8");
 
 function setup() {
     mic = new p5.AudioIn()
@@ -21,29 +19,26 @@ console.log(initialMessage)
 function draw() {
     micLevel = mic.getLevel();
 
-    if (micLevel >= 0.002) {
+    if (micLevel >= 0.0028) {
         initialMessage[0].style.opacity = 0;
-        flares.style.opacity = micLevel * 30;
-        leaf1.style.opacity = micLevel * 80;
-        unseen.style.opacity = micLevel * 20;
-        leaf2.style.opacity = micLevel * 70;
-        leaf3.style.opacity = micLevel * 60;
-        hear.style.opacity = micLevel * 20;
-        leaf4.style.opacity = micLevel * 50;
-        the.style.opacity = micLevel * 20;
-        leaf5.style.opacity = micLevel * 40;
-        background.style.opacity = micLevel * 100;
+        flares1.style.opacity = micLevel * 30;
+        flares2.style.opacity = micLevel * 20;
+        flares3.style.opacity = micLevel * 20;
+        mainPlanet.style.opacity = micLevel * 70;
+        mainPlanet.style.animationDuration = `${(micLevel * 10000) * .5}s`;
+        bluePlanet.style.opacity = micLevel * 70;
+        yellowPlanet.style.opacity = micLevel * 70;
+        purplePlanet.style.opacity = micLevel * 70;
+        clouds.style.opacity = micLevel * 20;
     } else {
         initialMessage[0].style.opacity = 1;
-        layer1.style.opacity = 0;
-        leaf1.style.opacity = 0;
-        unseen.style.opacity = 0;
-        leaf2.style.opacity = 0;
-        leaf3.style.opacity = 0;
-        hear.style.opacity = 0;
-        leaf4.style.opacity = 0;
-        the.style.opacity = 0;
-        leaf5.style.opacity = 0;
-        background.style.opacity = 0;
+        flares1.style.opacity = 0;
+        flares2.style.opacity = 0;
+        flares3.style.opacity = 0;
+        mainPlanet.style.opacity = 0;
+        bluePlanet.style.opacity = 0;
+        yellowPlanet.style.opacity = 0;
+        purplePlanet.style.opacity = 0;
+        clouds.style.opacity = 0;
     }
 }
